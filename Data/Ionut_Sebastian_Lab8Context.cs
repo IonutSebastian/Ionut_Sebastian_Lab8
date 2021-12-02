d2d12e5689d27031b3d287dac0ee41ef7be2c398
DESKTOP-7FLOC5V\KyleYG <iseby29@gmail.com>
2021-12-02 17:29:03 +0200

    commit

---
+++ Data/Ionut_Sebastian_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ionut_Sebastian_Lab8.Models;
+
+namespace Ionut_Sebastian_Lab8.Data
+{
+    public class Ionut_Sebastian_Lab8Context : DbContext
+    {
+        public Ionut_Sebastian_Lab8Context (DbContextOptions<Ionut_Sebastian_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ionut_Sebastian_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Ionut_Sebastian_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Ionut_Sebastian_Lab8.Models.Category> Category { get; set; }
+    }
+}
